1bed2bcdadf0b1fe71d2a49ab472edde33677a40
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-15 10:23:55 -0700

    working on sidebar

---
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -0,0 +1,16 @@
+import { TbUserSearch } from "react-icons/tb";
+
+const SearchInput = () => {
+  return (
+    
+      <form className="flex items-center gap-2">
+        <input type="text" placeholder="Search . . ." className="input input-bordered rounded-full" />
+        <button type="submit" className="btn btn-circle bg-blue-300 text-white">
+            <TbUserSearch className="w-6 h-6 outline-none"/>
+        </button>
+      </form>
+    
+  )
+}
+
+export default SearchInput
