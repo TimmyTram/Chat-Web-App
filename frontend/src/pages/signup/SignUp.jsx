b55a330ca188eb7e71c182aa03a114926d703003
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-15 09:50:18 -0700

    sign up mostly done

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-white-900 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-100">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Signup <span className="text-blue-700">to Chat</span>
+        </h1>
 
-export default SignUp
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Full Name"
+              className="w-full input input-bordered h-10 "
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered h-10 "
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered h-10 "
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-full input input-bordered h-10 "
+            />
+          </div>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-500 mt-2 inline-block"
+          >
+            Already have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">SIGNUP</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
