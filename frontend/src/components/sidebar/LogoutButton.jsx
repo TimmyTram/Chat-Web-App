b4e0cb06c4e0870bf189a11a1df19a71580b952a
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-26 09:15:23 -0700

    logout functional

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,11 +1,20 @@
 import { CgLogOut } from "react-icons/cg";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
   return (
     <div className="mt-auto">
-        <CgLogOut className="w-6 h-6 text-white cursor-pointer"/>
+      {!loading ? (
+        <CgLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
