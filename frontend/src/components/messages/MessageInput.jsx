313b2eb41e32dff6ecda18a8b366a45eac141ae7
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-27 12:48:07 -0700

    able to send messages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,16 +1,33 @@
 import { MdOutlineMessage } from "react-icons/md";
+import useSendMessage from "../../hooks/useSendMessage";
+import { useState } from "react";
 
 const MessageInput = () => {
+  
+  const [message, setMessage] = useState("");
+  const {loading, sendMessage} = useSendMessage();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    if(!message) return; // avoid spamming empty msgs
+
+    await sendMessage(message);
+    setMessage("");
+  }
+  
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Send Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button type='submit' className="absolute inset-y-0 end-0 flex items-center pe-3">
-            <MdOutlineMessage />
+            { loading ? <span className="loading loading-spinner"></span> : <MdOutlineMessage /> }
         </button>
       </div>
     </form>
