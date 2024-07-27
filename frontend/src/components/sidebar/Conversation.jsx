fc71fcd2975b36415d99e5f191f26a8bcd2c4aa1
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-27 12:32:06 -0700

    able to get conversations

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,7 +1,17 @@
-const Conversation = () => {
+import useConversation from "../../store/useConversation";
+
+const Conversation = ({ conversation, lastIndex, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-blue-300 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-blue-300 rounded p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-blue-300" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
             <img
@@ -12,15 +22,14 @@ const Conversation = () => {
         </div>
 
         <div className="flex flex-col flex-1">
-            <div className="flex gap-3 justify-between">
-                <p className="font-bold text-gray-500">Test User</p>
-                <span className="text-xl">😎</span>
-            </div>
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-500">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
+          </div>
         </div>
-
       </div>
 
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIndex && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
