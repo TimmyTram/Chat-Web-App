8f0273a4459bd418f252b76e1212c35a6e9e9e3d
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-07-27 22:01:19 -0700

    ????

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -1,15 +1,14 @@
-import { useState } from "react"
+import { useState } from "react";
 import useConversation from "../store/useConversation";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
-function useSendMessage() {
-  
-    const [loading, setLoading] = useState(false);
-    const {messages, setMessages, selectedConversation} = useConversation();
+const useSendMessage = () => {
+	const [loading, setLoading] = useState(false);
+	const { messages, setMessages, selectedConversation } = useConversation();
 
-    const sendMessage = async (message) => {
-        setLoading(true);
-        try {
+	const sendMessage = async (message) => {
+		setLoading(true);
+		try {
 			const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
 				method: "POST",
 				headers: {
@@ -17,21 +16,20 @@ function useSendMessage() {
 				},
 				body: JSON.stringify({ message }),
 			});
-
 			const data = await res.json();
+			if (data.error) throw new Error(data.error);
 			
-            if(data.error) throw new Error(data.error);
+			console.log(messages.then((messages) => (console.log([...messages, data]))));
 
-            setMessages({...messages, data});
+			messages.then((messages) => (setMessages([messages, data])));
+			//setMessages([...messages, data]);
 		} catch (error) {
 			toast.error(error.message);
-            console.log("[ERROR]: In useSendMessage")
 		} finally {
 			setLoading(false);
 		}
 	};
 
 	return { sendMessage, loading };
-}
-
-export default useSendMessage
+};
+export default useSendMessage;
