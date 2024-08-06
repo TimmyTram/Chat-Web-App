0f5a7e370381bad0beb74777aa1b9271d5876e3a
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-08-06 10:39:35 -0700

    able to render messages

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,6 +1,21 @@
-const Message = () => {
+import { useAuthContext } from '../../context/AuthContext';
+import useConversation from '../../store/useConversation';
+import { extractTime } from '../../utils/extractTime';
+
+const Message = ({message}) => {
+
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useConversation();
+
+  const fromMe = message.senderId === authUser._id;
+
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const bubbleBgColor = fromMe ? 'bg-blue-700' : '';
+
+  const formattedTime = extractTime(message.createdAt)
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
@@ -10,10 +25,10 @@ const Message = () => {
         </div>
       </div>
 
-      <div className={"chat-bubble text-white bg-blue-700"}>
-        User message here
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
       </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">00:00</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   );
 };
